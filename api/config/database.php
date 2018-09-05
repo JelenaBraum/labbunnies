1cd35c704a2c09236bbbb5735e620a5ca65812ae
Jelena Lenka Priplatova <jelena.soph@gmail.com>
2018-09-05 21:04:33 +0200

    Update database.php

--- api/config/database.php
+++ api/config/database.php
@@ -5,7 +5,7 @@ class Database{
     private $host = "localhost";
     private $db_name = "labbunnies";
     private $username = "labbunnies";
-    private $password = "129241252";
+    private $password = "***";
     public $conn;
  
     // get the database connection
