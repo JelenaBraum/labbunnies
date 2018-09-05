b1cdc9a725339be6c9f72362d4fa03df309903c8
Jelena Lenka Priplatova <jelena.soph@gmail.com>
2018-09-05 21:23:02 +0200

    Update database.sql

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Sep 05, 2018 at 01:39 PM
+-- Generation Time: Sep 05, 2018 at 09:22 PM
 -- Server version: 5.6.33-0ubuntu0.14.04.1
 -- PHP Version: 5.5.9-1ubuntu4.25
 
@@ -19,36 +19,32 @@ SET time_zone = "+00:00";
 --
 -- Table structure for table `regnumbers`
 --
--- Creation: Sep 02, 2018 at 01:40 PM
---
 
 CREATE TABLE IF NOT EXISTS `regnumbers` (
-  `BunnyID` int(11) NOT NULL AUTO_INCREMENT,
-  `hemail` varchar(255) COLLATE cp1250_czech_cs DEFAULT NULL,
-  `quests` text COLLATE cp1250_czech_cs,
-  PRIMARY KEY (`BunnyID`)
-) ENGINE=InnoDB DEFAULT CHARSET=cp1250 COLLATE=cp1250_czech_cs AUTO_INCREMENT=1 ;
+  `bunnyid` varchar(30) COLLATE utf8_bin NOT NULL,
+  `hemail` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `quests` text COLLATE utf8_bin,
+  PRIMARY KEY (`bunnyid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `regusers`
 --
--- Creation: Sep 05, 2018 at 06:51 AM
---
 
 CREATE TABLE IF NOT EXISTS `regusers` (
-  `BunnyID` int(8) NOT NULL,
-  `name` varchar(255) COLLATE cp1250_czech_cs NOT NULL,
-  `surname` varchar(255) COLLATE cp1250_czech_cs NOT NULL,
-  `email` varchar(255) COLLATE cp1250_czech_cs NOT NULL,
+  `bunnyidnum` int(8) NOT NULL,
+  `name` varchar(255) COLLATE utf8_bin NOT NULL,
+  `surname` varchar(255) COLLATE utf8_bin NOT NULL,
+  `email` varchar(255) COLLATE utf8_bin NOT NULL,
   `birthyear` int(4) NOT NULL,
-  `country` varchar(5) COLLATE cp1250_czech_cs NOT NULL,
-  `town` varchar(255) COLLATE cp1250_czech_cs NOT NULL,
-  `language` varchar(255) COLLATE cp1250_czech_cs NOT NULL,
+  `country` varchar(5) COLLATE utf8_bin NOT NULL,
+  `town` varchar(255) COLLATE utf8_bin NOT NULL,
+  `language` varchar(255) COLLATE utf8_bin NOT NULL,
   `toxo` int(1) NOT NULL,
-  `RhD` int(1) NOT NULL,
+  `rhd` int(1) NOT NULL,
   `emailcheck` int(11) NOT NULL,
-  `notes` text COLLATE cp1250_czech_cs NOT NULL,
-  PRIMARY KEY (`BunnyID`)
-) ENGINE=InnoDB DEFAULT CHARSET=cp1250 COLLATE=cp1250_czech_cs;
+  `notes` text COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`bunnyidnum`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
